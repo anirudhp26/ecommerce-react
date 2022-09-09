4a1638b495f08b58044cd5cbd5f83d923ec10bf6
anirudhp26 <anirudh26p@gmail.com>
2022-09-09 12:50:38 +0530

    development

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+      <Navbar />
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,37 @@
 import React from "react";
+import '../css/main.css'
 
 export default function Navbar() {
     return (
         <>
-        <div> 
-        </div>
+            <nav>
+                <span class="material-symbols-outlined">
+                    menu
+                </span>
+                <div className="navbar-icon">
+                    {/* img */}
+                    <a href="/">ECOMMERCE-STORE</a>
+                </div>
+                <div className="navbar-searchbox">
+                    <input type='text' placholder='Search' className='search-box' />
+                </div>
+                <div className="navbar-items">
+                    <ul>
+                        <li><span class="material-icons">
+                            home
+                        </span>HOME</li>
+                        <li><span class="material-icons">
+                            add_shopping_cart
+                        </span>CART</li>
+                        <li>DEALS</li>
+                    </ul>
+                </div>
+                <div className="navbar-user">
+                    <a href="/"><span class="material-icons">
+                        account_circle
+                    </span>USER</a>
+                </div>
+            </nav>
         </>
     )
 }
